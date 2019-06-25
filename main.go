144b21ddcf9045116bf4357701b91e1f31e463b0
Elio Quevedo <heliosqs17@gmail.com>
2019-06-25 01:28:04 -0500

    Create an independent directory for providers, datasources, resources and tests.

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/TimDurward/terraform-provider-slack/slack"
 	"github.com/hashicorp/terraform/plugin"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: Provider,
+		ProviderFunc: slack.Provider,
 	})
 }
